9ce09e939b53fc60bb8d1c6891f0e4bb7be9e381
Adam Pavlik <adam.pavlik@exxeta.com>
2025-03-25 14:30:52 +0100

    Add a Streamlit Web UI. Finish the RAG workshop

--- ragbot.py
+++ ragbot.py
@@ -1,5 +1,6 @@
 from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.core.chat_engine.types import ChatMode
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 
 API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBkY2M5YzIwYjI5Y2UzNjY0NzY2ZjhhNDEwNDRmM2JlIn0.mIIt1JQSSquMS5Quae00cn4-xP_mQCWVe1olx8jkIUY"
@@ -14,6 +15,8 @@ class Ragbot:
     EMBED_API_VERSION = "2023-05-15"
     EMBED_MODEL_NAME = "text-embedding-ada-002"
     EMBED_DEPLOYMENT_NAME = "text-embedding-ada-002"
+
+    STORAGE_CONTEXT_PATH = ""
 
     question = "What is Sora?"
 
@@ -39,17 +42,32 @@ class Ragbot:
     def ingest(self, input_files: dict):
         docs = SimpleDirectoryReader(input_files=input_files).load_data()
         print(f'Ragbot > Ingest: nr. of docs: {len(docs)}')
-        index = VectorStoreIndex.from_documents(documents=docs, embed_model=self.exxeta_embedding)
-        self.query_engine = index.as_query_engine(llm=self.exxeta_ai_llm)
+        self.index = VectorStoreIndex.from_documents(documents=docs, embed_model=self.exxeta_embedding)
+        self.query_engine = self.index.as_query_engine(llm=self.exxeta_ai_llm)
+        self.chat_engine = self.index.as_chat_engine(chat_mode=ChatMode.CONDENSE_QUESTION, verbose=True, llm=self.exxeta_ai_llm)
 
-    def answer_question(self, question: str):
+
+    def query(self, question: str):
         answer = self.query_engine.query(question)
         print(f'{question} : {answer.response}')
+        return answer.response
+
+
+    def chat(self, question: str, history: list):
+        answer = self.chat_engine.chat(question, chat_history=history)
+        print(f'{question} - {answer.response}')
+        return answer.response
     
 
-ragbot = Ragbot()
-ragbot.ingest(['docs/Sora.pdf'])
-ragbot.answer_question('What is Sora?')
+def ragbot_test():
+    print('Creating new Ragbot instance')
+    ragbot = Ragbot()
+    
+    print('Ragbot is ingesting PDF doc "Sora.pdf"')
+    ragbot.ingest(['docs/Sora.pdf'])
+
+    print('Ragbot is looking for an answer to the question "What is Sora?"')
+    ragbot.query('What is Sora?')
 
 
 
